85f7b3ce170e482f4ac5af6119722141a2f90e85
Zion-art802 <zionchinedu32@gmail.com>
2025-01-06 11:13:03 +0100

    Update scripts.js

--- New projectFile/scripts.js
+++ New projectFile/scripts.js
@@ -3,7 +3,7 @@ const signInButton=document.getElementById('signInButton');
 const signInForm=document.getElementById('signInForm');
 const signUpForm=document.getElementById('signUpForm');
 
-signUpButton.addEventListener('click',function fish () {
+signUpButton.addEventListener('click',function() {
     signInForm.style.display="none";
     signUpForm.style.display="block";
 })
